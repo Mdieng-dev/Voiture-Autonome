bb0793628078e17770e92958700c3be5aef4a1a1
ClemGir <clem.gir@gmx.fr>
2026-04-08 16:20:36 +0200

    Update capteurUS_I2C_RTOS.c
    mise a jour du code, ajout du multi capteurs et de la transmission CAN

--- software/Détection-Obstacle/capteurUS_I2C_RTOS.c
+++ software/Détection-Obstacle/capteurUS_I2C_RTOS.c
@@ -1,168 +1,234 @@
-/*----------------------------------------------------------------------------
- * CMSIS-RTOS 'main' function template - OPTIMISÉ TEMPS RÉEL
- *---------------------------------------------------------------------------*/
- 
 #include "RTE_Components.h"
 #include  CMSIS_device_header
 #include "cmsis_os2.h"
 #include "stm32f4xx.h"                  // Device header
 #include "Driver_I2C.h"                 // CMSIS Driver:I2C
+#include "Driver_CAN.h"                 // CMSIS Driver:CAN
 #include "RTE_Device.h"                 // Device:STM32Cube Framework:Classic
 #include "rtx_os.h"                     // CMSIS:RTOS2:Keil RTX5&&Source
 #include "EventRecorder.h"              // CMSIS-View:Event Recorder&&DAP
-#include "stm32f4xx_hal.h"              // Device:STM32Cube HAL:Common
 
 extern ARM_DRIVER_I2C        Driver_I2C1;
 #define I2C_A                (&Driver_I2C1)
 
-// Définition du nombre de capteurs et de leurs adresses
-#define NB_CAPTEURS 3
-const uint8_t ADRESSES_I2C[NB_CAPTEURS] = {(0xE0>>1),(0xE2>>1),(0xE4>>1),(0xE8>>1)};
+extern ARM_DRIVER_CAN Driver_CAN2;
 
-// Structure du message envoyé dans }a Mailbox
-typedef struct {
-    uint8_t id_capteur;  // 0 pour le premier, 1 pour le deuxième, etc.
-    uint16_t distance;   // Valeur mesurée
-} MessageMesure_t;
+osThreadId_t ID_tacheI2C,ID_tacheCAN;
+osThreadId_t ID_tacheLED; 
+
+osMutexId_t ID_Mutex_Data; 
 
 
-
-osThreadId_t ID_tacheI2C;
-osThreadId_t ID_tacheLED;
-//ID BAL
-osMessageQueueId_t ID_BAL;
-
-// Définition d'un flag pour l'événement I2C
 #define I2C_EVENT_DONE 0x01
+#define CAN_EVENT_DONE 0x01
+
+
+#define NB_CAPTEURS 4 // Modifié à 3 selon ta description textuelle
+const uint8_t ADRESSES_I2C[NB_CAPTEURS] = {(0xE0>>1), (0xE2>>1), (0xE4>>1),(0xE8>>1)};
+
+uint16_t distance_global[NB_CAPTEURS];
+/*----------------------------------------------------------------------------
+ * Fonction de Callback I2C
+ *---------------------------------------------------------------------------*/
+
+
+void callbackI2C(uint32_t event) {
+    if (event & ARM_I2C_EVENT_TRANSFER_DONE) {
+        osThreadFlagsSet(ID_tacheI2C, I2C_EVENT_DONE);
+    }
+}
+
+void callbackCAN(uint32_t obj_idx, uint32_t event) {
+	if (event & ARM_CAN_EVENT_SEND_COMPLETE) {
+		osThreadFlagsSet(ID_tacheCAN, CAN_EVENT_DONE); 
+	}
+}
 
 /*----------------------------------------------------------------------------
- * Fonction de Callback I2C (Exécutée sous interruption)
- * Réveille la tâche I2C quand la transmission/réception est terminée
+ * Fonctions d'Initialisation
  *---------------------------------------------------------------------------*/
-void I2C_SignalEvent(uint32_t event) {
-    // Si le transfert est terminé avec succès
-    if (event & ARM_I2C_EVENT_TRANSFER_DONE) {
-        osThreadFlagsSet(ID_tacheI2C, I2C_EVENT_DONE); // Envoie le flag pour réveiller la tâche
+void init_LED(void){
+    __HAL_RCC_GPIOD_CLK_ENABLE(); 
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15; 
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;   
+    GPIO_InitStruct.Pull = GPIO_NOPULL;            
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;  
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+}
+
+void init_I2C(void){
+    Driver_I2C1.Initialize(callbackI2C); 
+    Driver_I2C1.PowerControl(ARM_POWER_FULL);
+    Driver_I2C1.Control(ARM_I2C_BUS_SPEED, ARM_I2C_BUS_SPEED_STANDARD); 
+}
+
+void init_CAN(void){
+	Driver_CAN2.Initialize(NULL, callbackCAN);
+	Driver_CAN2.PowerControl(ARM_POWER_FULL);
+	Driver_CAN2.SetMode(ARM_CAN_MODE_INITIALIZATION);
+	Driver_CAN2.SetBitrate( ARM_CAN_BITRATE_NOMINAL, // débit fixe
+													125000, // 125 kbits/s (LS)
+													ARM_CAN_BIT_PROP_SEG(3U) | // prop. seg = 3 TQ
+													ARM_CAN_BIT_PHASE_SEG1(1U) | // phase seg1 = 1 TQ
+													ARM_CAN_BIT_PHASE_SEG2(1U) | // phase seg2 = 1 TQ
+													ARM_CAN_BIT_SJW(1U) // Resync. Seg = 1 TQ
+													);
+	
+	Driver_CAN2.ObjectConfigure(2,ARM_CAN_OBJ_TX); 
+	Driver_CAN2.SetMode(ARM_CAN_MODE_NORMAL); 
+}
+
+/*----------------------------------------------------------------------------
+* Tache I2C 
+ *---------------------------------------------------------------------------*/
+static void tache_I2C (void *argument) {
+    uint8_t addr1 = ADRESSES_I2C[1]; 
+    uint8_t addr2 = ADRESSES_I2C[3];
+    uint8_t reg_cmd[2] = {0x00, 0x51};
+    uint8_t reg_msb = 0x02, reg_lsb = 0x03;
+    uint8_t vMSB, vLSB;
+    uint16_t dist;
+		
+    while(1) {
+        // --- LECTURE CAPTEUR 1 ---
+        Driver_I2C1.MasterTransmit(addr1, reg_cmd, 2, false);
+        osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever);
+        osDelay(70);
+        
+        Driver_I2C1.MasterTransmit(addr1, &reg_msb, 1, true);
+        osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever);
+        Driver_I2C1.MasterReceive(addr1, &vMSB, 1, false);
+        osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever);
+        
+        Driver_I2C1.MasterTransmit(addr1, &reg_lsb, 1, true);
+        osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever);
+        Driver_I2C1.MasterReceive(addr1, &vLSB, 1, false);
+        osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever);
+
+        dist = (vMSB << 8) | vLSB;
+				
+				//ecriture le la valeur mesurée, du capteur 1, a l'aide du mutex
+				osMutexAcquire(ID_Mutex_Data, osWaitForever);
+        distance_global[1] = dist; 
+        osMutexRelease(ID_Mutex_Data);
+				
+			
+        // --- LECTURE CAPTEUR 2 ---
+        Driver_I2C1.MasterTransmit(addr2, reg_cmd, 2, false);
+        osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever);
+        osDelay(70);
+        
+        Driver_I2C1.MasterTransmit(addr2, &reg_msb, 1, true);
+        osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever);
+        Driver_I2C1.MasterReceive(addr2, &vMSB, 1, false);
+        osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever);
+        
+        Driver_I2C1.MasterTransmit(addr2, &reg_lsb, 1, true);
+        osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever);
+        Driver_I2C1.MasterReceive(addr2, &vLSB, 1, false);
+        osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever);
+
+        dist = (vMSB << 8) | vLSB; // On réutilise dist
+        
+				//ecriture le la valeur mesurée, du capteur 2, a l'aide du mutex
+				osMutexAcquire(ID_Mutex_Data, osWaitForever);
+        distance_global[3] = dist;  
+        osMutexRelease(ID_Mutex_Data);
+
+        osDelay(100);
     }
 }
 
 /*----------------------------------------------------------------------------
- * Tache I2C : mesure de distance capteurUS (cm)
+ * Tache LED 
  *---------------------------------------------------------------------------*/
-static void tache_I2C (void *argument) {
+static void tache_LED (void *argument) {
+    uint16_t msg_recu; // Réception de la structure
+    
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15, GPIO_PIN_RESET);
 
-		static uint8_t index = 0;
-		uint8_t reg[10], regMSB[10], regLSB[10];
-		uint8_t val_LSB, val_MSB;
-		MessageMesure_t message;
-
-
-		reg[0] = 0x00; // registre de commande 
-		reg[1] = 0x51; // commande de mesure en cm
-
-		//registre de mesure
-		regMSB[0] = 0x02; // registre de mesure bit de poids fort
-		regLSB[0] = 0x03; // registre de mesure bit de poids faible
-
-		while(1) {
+    
+    while(1) {
+				osMutexAcquire(ID_Mutex_Data, osWaitForever);
+        msg_recu = distance_global[1];  
+        osMutexRelease(ID_Mutex_Data);
+            
+				// Eteint les LEDs dédiées au capteur 1 (ex: Rouge et Orange)
+				HAL_GPIO_WritePin(GPIOD,GPIO_PIN_13 | GPIO_PIN_14, GPIO_PIN_RESET);
 				
-			uint8_t addr_slv = ADRESSES_I2C[index];
-			
-			// 1. Commande pour demander la mesure
-			Driver_I2C1.MasterTransmit(addr_slv, reg, 2, false);
-			osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever); // Attente passive
+				if(msg_recu > 0x0010) HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);// Rouge
+				if(msg_recu > 0x0020) HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);// Orange
+             
+            
+				osMutexAcquire(ID_Mutex_Data, osWaitForever);
+        msg_recu = distance_global[3];  
+        osMutexRelease(ID_Mutex_Data);
 
-			osDelay(65); // 65ms d'attente passive pour laisser le capteur mesurer
-
-			// 2. Lire le MSB
-			Driver_I2C1.MasterTransmit(addr_slv, regMSB, 1, true); // true = sans stop
-			osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever); // Attente passive
-
-			Driver_I2C1.MasterReceive(addr_slv, &val_MSB, 1, false); // false = avec stop
-			osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever); // Attente passive
-
-			// 3. Lire le LSB
-			Driver_I2C1.MasterTransmit(addr_slv, regLSB, 1, true); // true = sans stop
-			osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever); // Attente passive
-
-			Driver_I2C1.MasterReceive(addr_slv, &val_LSB, 1, false); // false = avec stop
-			osThreadFlagsWait(I2C_EVENT_DONE, osFlagsWaitAny, osWaitForever); // Attente passive
-
-
-			// Sauvegarde des 2 octets mesurés sous une variable de 16 bits
-			message.distance = (val_MSB << 8) + val_LSB;
-			message.id_capteur = addr_slv;
-			// Envoie la valeur mesurée sur 16 bits à la mailbox (La priorité est à 0, pas NULL)
-			osMessageQueuePut(ID_BAL, &message, 0, osWaitForever);
-
-
-			// Passage au capteur suivant
-			index = (index + 1) % NB_CAPTEURS;
-			osDelay(100);
-		}
+				// Eteint les LEDs dédiées au capteur 2 (ex: Verte et Bleue)
+				HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12 | GPIO_PIN_15, GPIO_PIN_RESET);
+				
+				if(msg_recu > 0x0010) HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);// Verte
+				if(msg_recu > 0x0020) HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET); // Bleue
+            
+    osDelay(100); 
+		} 
 }
 
 /*----------------------------------------------------------------------------
- * Tache LED : affichage distance 
+ * Tache CAN 
  *---------------------------------------------------------------------------*/
-static void tache_LED (void *argument) {
-    MessageMesure_t message;
-    osStatus_t status;
-		uint8_t ID=0;
-    
+static void tache_CAN (void *argument) {
+	ARM_CAN_MSG_INFO tx_msg_info;
+	
+	uint16_t msg_recu;
+	uint8_t data[2];
+	
+	tx_msg_info.id = ARM_CAN_STANDARD_ID(0x110); // ID CAN
+	tx_msg_info.rtr = 0; // 0 = trame DATA
+	
+	while (1) {
+		// Envoie de la valeur mesuré du capteur 1
+		osMutexAcquire(ID_Mutex_Data, osWaitForever);
+		msg_recu = distance_global[1];  
+		osMutexRelease(ID_Mutex_Data);
 		
-    while(1) {
-			// La tâche s'endort ici jusqu'à recevoir un message
-			status = osMessageQueueGet(ID_BAL, &message, NULL, osWaitForever);						
-			if (status == osOK) {
-				if(message.id_capteur==ADRESSES_I2C[0]){
-					HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15, GPIO_PIN_RESET);
-					
-					// Affichage de la distance
-					if(message.distance > 0x0025) HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
-					if(message.distance > 0x0020) HAL_GPIO_WritePin(GPIOD, GPIO_PIN_12, GPIO_PIN_SET);
-					if(message.distance > 0x0015) HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
-					if(message.distance > 0x0010) HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
-				}					
-			}
-		}
+		data[0]=(msg_recu>>8);
+		data[1]=msg_recu;
+		
+		// envoi CAN
+		Driver_CAN2.MessageSend(2, &tx_msg_info, data, 2);
+		// Sommeil attente fin envoi sur EV1
+		osThreadFlagsWait(CAN_EVENT_DONE,osFlagsWaitAny, osWaitForever);
+		
+		//envoie periodique toute les 50ms
+		osDelay(500);
+	}
 }
 
 osThreadAttr_t configT1 = {.priority = osPriorityAboveNormal};
+osThreadAttr_t configT2 = {.priority = osPriorityBelowNormal};
 
 int main (void) {
-    // System Initialization
     SystemCoreClockUpdate();
-    EventRecorderInitialize (EventRecordAll, 1);
     
-		//initialisation LEDs
-		__HAL_RCC_GPIOD_CLK_ENABLE(); 
+		//Initialisations
+		init_CAN();
+    init_I2C();
+		init_LED();
     
-    GPIO_InitTypeDef GPIO_InitStruct = {0};
-    GPIO_InitStruct.Pin = GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15; 
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;   
-    GPIO_InitStruct.Pull = GPIO_NOPULL;           
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;  
-    
-    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
-	
-    // Initialisation I2C avec ajout du CALLBACK pour supprimer l'attente active
-    Driver_I2C1.Initialize(I2C_SignalEvent); 
-    Driver_I2C1.PowerControl(ARM_POWER_FULL);
-    Driver_I2C1.Control(ARM_I2C_BUS_SPEED, ARM_I2C_BUS_SPEED_STANDARD); // 100 kHz                                
-    
-    // Initialize mailbox (correction de la signature de osMessageQueueNew)
-    ID_BAL = osMessageQueueNew(4, sizeof(MessageMesure_t), NULL); // File de 4 messages de 2 octets
-
-    osKernelInitialize(); // Initialize CMSIS-RTOS
-    
-    // Création des threads (la signature standard demande void *argument, géré plus haut)
+    osKernelInitialize();
+		
+		ID_Mutex_Data = osMutexNew(NULL);
+		
+    //Initialisation des tache
     ID_tacheI2C = osThreadNew(tache_I2C, NULL, &configT1);
-    ID_tacheLED = osThreadNew(tache_LED, NULL, &configT1);
+		ID_tacheCAN = osThreadNew(tache_CAN, NULL, &configT1);
+    ID_tacheLED = osThreadNew(tache_LED, NULL, &configT2);
     
-    osKernelStart(); // Start thread execution
+		
+		
+    osKernelStart();
     
-    for (;;) {} // Fallback classique du main
+    for (;;) {} 
 }
-
-
